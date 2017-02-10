d4b24d5d9a92839b3bcc60bf2e05e06aa770a612
Christian Ejercito <cejercit@uci.edu>
2017-02-10 15:01:30 -0800

    client

---
+++ client/client.js
@@ -0,0 +1,42 @@
+var clientAddress = "127.0.0.1";
+var clientPort = "21234";
+
+function Socket(model){
+	this.model = model;
+	this.connection = new WebSocket('ws://'+clientAddress+':'+clientPort, ['soap', 'xmpp']);
+	
+
+
+// Log errors
+	this.scoreArray = [0,0];
+	this.count = 1;
+	this.connection.onerror = function (error) {
+		console.log('WebSocket Error ' + error);
+	};
+
+// Log messages from the server
+	this.connection.onmessage = (e)=> {
+		//this is in scope?
+		var array = e.data.split(":");
+		if(this.count)
+		{
+		//assumes client 0 based ids
+			var ids = this.model.getIds();
+			
+			console.log("init:"+ids[0]+":"+ids[1]);
+			this.connection.send("init:"+ids[0]+":"+ids[1]);
+		}
+		console.log(e.data)
+		this.scoreArray[0] = array[0];
+		this.scoreArray[1] = array[1];
+		this.model.setScore(this.scoreArray);
+		this.count =0;
+		ViewRefresh();
+	}
+	this.sendMessage = function(inc)
+	{
+		this.connection.send(inc);
+	}
+
+	this.done = ()=>{this.connection.send("DONE")}
+};
