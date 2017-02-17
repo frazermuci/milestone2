afd09d40764731642710cc8ea841cd76b1a65875
Christian Ejercito <cejercit@uci.edu>
2017-02-17 13:59:22 -0800

    de/serialization; init

--- client/client.js
+++ client/client.js
@@ -26,10 +26,24 @@ function Socket(model){
 			console.log("init:"+ids[0]+":"+ids[1]);
 			this.connection.send("init:"+ids[0]+":"+ids[1]);
 		}
+		if (array[0] == "init"
+		{
+			array[1] = model.snakeID;
+			sendMessage("init:" + model.snakeID);
+		}
+		else 
+		{
+			deserialize(array[0]);
+			sendMessage(serialize(model));
+		}
 		console.log(e.data)
-		this.scoreArray[0] = array[0];
-		this.scoreArray[1] = array[1];
-		this.model.setScore(this.scoreArray);
+		//{
+			//deserialize(array);
+			//sendMessage(serialize(model));
+		//}
+		//this.scoreArray[0] = array[0];
+		//this.scoreArray[1] = array[1];
+		//this.model.setScore(this.scoreArray);
 		this.count =0;
 		ViewRefresh();
 	}
@@ -37,6 +51,141 @@ function Socket(model){
 	{
 		this.connection.send(inc);
 	}
+	
+	this.deserialize = function(s)
+	{
+		var a = s[0];
+		var s1Dir = Vector(1,0);
+		var s1Bonus = false;
+		var s1Loss = false;
+		var s2Dir=Vector(1,0);
+		var s2Bonus=false;
+		var s2Loss=false;
+		var s1BonusX = 0;
+		var s1BonusY = 0;
+		var s2BonusX = 0;
+		var s2BonusY = 0;
+    
+    // s1Dir s1Dir s1Bonus s1Loss s2Dir s2Dir s2Bonus s2Loss
+    if(a > 127)
+    {
+        a -= 128;
+        if(a > 63) // Up
+        {
+            a-=64;
+            s1Dir = Vector(0,-1);
+        }
+        else // Down
+            s1Dir = Vector(0, 1);
+    }
+    else
+    {
+        if(a > 63) // Right
+        {
+            a-=64;
+            s1Dir = Vector(1,0);
+        }
+        else // Left
+            s1Dir = Vector(-1, 0);
+    }
+    
+    if(a > 31)
+    {
+        a-= 32;
+        s1Bonus = true;
+    }
+    
+    if(a > 15)
+    {
+        a-= 16;
+        s1Loss = true;
+    }
+    
+    if(a > 7)
+    {
+        a -= 8;
+        if(a > 3) // Up
+        {
+            a-=4;
+            s2Dir = Vector(0,-1);
+        }
+        else // Down
+            s2Dir = Vector(0, 1);
+    }
+    else
+    {
+        if(a > 3) // Right
+        {
+            a-=4;
+            s2Dir = Vector(1,0);
+        }
+        else // Left
+            s2Dir = Vector(-1, 0);
+    }
+    
+    if(a > 1)
+    {
+        a-= 2;
+        s2Bonus = true;
+    }
+    
+    if(a > 0)
+    {
+        a-= 1;
+        s2Loss = true;
+    }
+    
+    var i = 1;
+    
+    if(s1Bonus)
+    {
+        var b = s[i];
+        s1BonusY = b%16;
+        s1BonusX = (b-s1BonusY)/16;
+        i++;
+    }
+    if(s2Bonus)
+    {
+        var c = s[i];
+        s2BonusY = c%16;
+        s2BonusX = (c-s2BonusY)/16;
+        i++;
+    }
+	 getModel().changeDirection(0, s1Dir);
+    getModel().changeDirection(1, s2Dir);
+    getModel().growSnake(0);
+    getModel().growSnake(1);
+    if(s1Bonus)
+    {
+        getModel().getSnake(0).eatBonus();
+        
+        var bonusToChange = 1;
+        var newBonusPos = Vector(s1BonusX, s1BonusY);
+        var snake1Head = getModel().getSnake(0).getHead();
+        
+        if(snake1Head.equals(getModel.getBonusPos(0)))
+            bonusToChange = 0;
+        
+        getModel().setBonus(bonusToChange,newBonusPos);
+    }
+	}
+	
+	this.serialize = function(model)
+	{
+		
+    var snake = getModel().getSnake(model.snakeID); // TODO NEEDS TO KNOW WHAT SNAKE
+    var dir = snake.direction;
+    
+    if(dir.equals(Vector(1,0))) // Right
+        return 64+16+4+1; // 01 01 01 01
+    if(dir.equals(Vector(0,-1))) // Up
+        return 128+64+32+16+8+4+2+1; // 11 11 11 11
+    if(dir.equals(Vector(-1,0))) // Left
+        return 0; // 00 00 00 00
+    if(dir.equals(Vector(0,1))) // Down
+        return 128+32+8+2; // 01 01 01 01
+}
+
 
 	this.done = ()=>{this.connection.send("DONE")}
 };
